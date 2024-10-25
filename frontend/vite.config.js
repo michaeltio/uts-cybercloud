b457b95483bb0e48e5b1152f1d5c47f760300978
michaeltio <tio.michael2004@gmail.com>
2024-10-25 15:37:29 +0700

    tambahin vite seeting

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const base = process.env.CUSTOM_PATH ? `/${process.env.CUSTOM_PATH}/` : '/michael-tio';
+
 export default defineConfig({
+  base,
   plugins: [vue()],
 })
